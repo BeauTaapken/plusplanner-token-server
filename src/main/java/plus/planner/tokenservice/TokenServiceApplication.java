83dc0d13a39af9e04504ec563621dd1c505d2532
Niek125 <niek.sleddens@gmail.com>
2019-12-09 11:01:01 +0100

    eureka client

--- src/main/java/plus/planner/tokenservice/TokenServiceApplication.java
+++ src/main/java/plus/planner/tokenservice/TokenServiceApplication.java
@@ -2,11 +2,13 @@ package plus.planner.tokenservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@EnableEurekaClient
 @SpringBootApplication
 public class TokenServiceApplication {
 
