7451f731f18ed68430cb3e04375744aafbec2bc3
Niek125 <niek.sleddens@gmail.com>
2019-12-14 14:12:44 +0100

    response status

--- src/main/java/plus/planner/tokenservice/controllers/TokenController.java
+++ src/main/java/plus/planner/tokenservice/controllers/TokenController.java
@@ -4,11 +4,9 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import plus.planner.tokenservice.models.Role;
 import plus.planner.tokenservice.models.UserData;
@@ -39,14 +37,18 @@ public class TokenController {
     }
 
     @RequestMapping("/gettoken")
-    public String getNewToken(@RequestHeader("FToken") String ftoken) throws JsonProcessingException {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer "+ ftoken);
-        HttpEntity<String> entity = new HttpEntity<String>(headers);
-        RestTemplate r = new RestTemplate();
-        ResponseEntity<UserData> userData = r.exchange("https://api.fhict.nl/people/me", HttpMethod.GET, entity, UserData.class);
-        Role[] permissions = restTemplate.getForObject("http://plus-planner-role-management-service/role/read/" + "sdfghjk", Role[].class);
-        return generator.getNewToken(userData.getBody(), permissions);
+    public ResponseEntity getNewToken(@RequestHeader("FToken") String ftoken) throws JsonProcessingException {
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Authorization", "Bearer " + ftoken);
+            HttpEntity<String> entity = new HttpEntity<String>(headers);
+            RestTemplate r = new RestTemplate();
+            ResponseEntity<UserData> userData = r.exchange("https://api.fhict.nl/people/me", HttpMethod.GET, entity, UserData.class);
+            Role[] permissions = restTemplate.getForObject("http://plus-planner-role-management-service/role/read/" + "sdfghjk", Role[].class);
+            return new ResponseEntity(generator.getNewToken(userData.getBody(), permissions), HttpStatus.ACCEPTED);
+        } catch (HttpClientErrorException e) {
+            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+        }
     }
 
 }
