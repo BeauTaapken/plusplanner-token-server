5ccbe3b9dfe35d7baf4dbef57be4da7a89742f0c
Niek125 <niek.sleddens@gmail.com>
2020-01-22 14:30:09 +0100

    adding users

--- src/main/java/plus/planner/tokenservice/controllers/TokenController.java
+++ src/main/java/plus/planner/tokenservice/controllers/TokenController.java
@@ -4,7 +4,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import plus.planner.tokenservice.models.Role;
 import plus.planner.tokenservice.models.UserData;
@@ -33,6 +36,11 @@ public class TokenController {
         final RestTemplate r = new RestTemplate();
         logger.info("sending fontys request");
         final ResponseEntity<UserData> userData = r.exchange("https://api.fhict.nl/people/me", HttpMethod.GET, entity, UserData.class);
+        logger.info("adding user");
+        final HttpEntity httpEntity = new HttpEntity("{\"userid\":\"" + userData.getBody().getUid() +
+                "\",\"username\":\"" + userData.getBody().getDisplayName() +
+                "\",\"photo\":\"" + userData.getBody().getPhoto() + "\"}");
+        restTemplate.postForObject("https://plus-planner-role-management-service/user/save", httpEntity, UserData.class);
         logger.info("getting roles for userid: " + userData.getBody().getUid());
         final Role[] permissions = restTemplate.getForObject("https://plus-planner-role-management-service/role/read/" + userData.getBody().getUid(), Role[].class);
         logger.info("generating token");
